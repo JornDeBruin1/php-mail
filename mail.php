68b9c55c9fb80c8c6be870afd2c88bdbe0eeb4bc
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-03-19 11:39:34 +0100

    different mail docent

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $message = "Test Bericht van Jorn de Bruin";
 $email = "jorn.bruin@student.graafschapcollege.nl"; // sender mail address use your own!
 
 // Recipient email address
-$to = 'p.veltman@graafschapcollege.nl';
+$to = 'net24pveltman@net24pveltman.gc-webhosting.nl';
 // $to = 'jorndebruin@outlook.com';
 
         // Create email headers
